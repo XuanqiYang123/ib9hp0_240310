9f41501ae24374297017fab47d7ef9ccda36aa3c
XuanqiYang123 <160729713+XuanqiYang123@users.noreply.github.com>
2024-03-12 16:36:44 +0000

    Update transformation.R
    Signed-off-by: XuanqiYang123 <160729713+XuanqiYang123@users.noreply.github.com>

--- R/transformation.R
+++ R/transformation.R
@@ -12,35 +12,8 @@ for (file in data_files) {
 }
 print("Write them to the database")
 
-# 检查data_to_write是否为空
-if(nrow(data_to_write) == 0) {
-  stop("data_to_write is empty.")
-}
 
-# 检查data_to_write的结构
-print(str(data_to_write))
-
-# 尝试连接数据库
 connection <- RSQLite::dbConnect(RSQLite::SQLite(), "database/products.db")
-if(is.null(connection)) {
-  stop("Failed to connect to the database.")
-}
-
-# 写入数据前的最后检查
-if(!dbExistsTable(connection, "products")) {
-  print("Table 'products' does not exist. It will be created.")
-} else if(overwrite) {
-  print("Table 'products' will be overwritten.")
-}
-
-# 执行写入操作
-RSQLite::dbWriteTable(connection, "products", data_to_write, overwrite = TRUE)
-
-# 断开连接
+RSQLite::dbWriteTable(connection, "products", data_to_write, append = TRUE)
 RSQLite::dbDisconnect(connection)
 print("Done!")
-
-#connection <- RSQLite::dbConnect(RSQLite::SQLite(), "database/products.db")
-#RSQLite::dbWriteTable(connection, "products", data_to_write, overwrite = TRUE)
-#RSQLite::dbDisconnect(connection)
-#print("Done!")
