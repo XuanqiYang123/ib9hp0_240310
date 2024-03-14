c7c2c52167eb941bb532c38b4757ebba61671068
Fadhilah Salsabil Minako <dila@Fadhilahs-MacBook-Air.local>
2024-03-14 20:20:20 +0000

    inserting data into the database

--- IB9HP0_9_Data_Insertion_Analysis.R
+++ IB9HP0_9_Data_Insertion_Analysis.R
@@ -84,4 +84,20 @@ advertisers_table <- advertisers_file
 ##Normalising Advertisement Table
 advertisements_table <- advertisements_file
 
+# Create connection to SQL database
+db_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"IB9HP0_9.db")
+
+# Inserting Dataframe into the sql database
+
+## Inserting Products table
+dbWriteTable(db_connection,"products",products_table, append = TRUE)
+
+## Inserting Reviews table
+dbWriteTable(db_connection,"reviews",reviews_table, append = TRUE)
+
+## Inserting Memberships table
+dbWriteTable(db_connection,"memberships",memberships_table, append = TRUE)
+
+## Inserting Memberships table
+dbWriteTable(db_connection,"memberships",memberships_table, append = TRUE)
 
